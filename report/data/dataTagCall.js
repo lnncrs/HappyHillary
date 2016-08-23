101024c9aa963e0b6fa3c9c2d27217fcb962df62
lenin cristi <lenincristi@gmail.com>
2016-08-22 21:23:37 -0300

    modelos alterados

--- report/data/dataTagCall.js
+++ report/data/dataTagCall.js
@@ -1,4 +1,4 @@
-$('#tg_subj_201505').jQCloud(subj_201505);
-$('#tg_subj_201506').jQCloud(subj_201506);
-$('#tg_subj_201507').jQCloud(subj_201507);
-$('#tg_subj_201508').jQCloud(subj_201508);
+$('#tg_r01g01').jQCloud(tg_r01g01_data);
+$('#tg_r01g02').jQCloud(tg_r01g02_data);
+$('#tg_r01g03').jQCloud(tg_r01g03_data);
+$('#tg_r01g04').jQCloud(tg_r01g04_data);
