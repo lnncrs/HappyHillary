c368689bae49e97de14a0a70ff8a591322e69373
lenin cristi <lenincristi@gmail.com>
2016-08-23 16:07:44 -0300

    teste.scm atualizado

--- modules/test-l.scm
+++ modules/test-l.scm
@@ -6,39 +6,77 @@
 (load-relative "modules/io/writer.scm")
 (load-relative "modules/workers/counter.scm")
 
-(define files '("data/leak/leak_subj_201505.txt"
-                "data/leak/leak_subj_201506.txt"
-                "data/leak/leak_subj_201507.txt"
-                "data/leak/leak_subj_201508.txt"))
+;(define files '("data/leak/leak_subj_201505.txt"
+;                "data/leak/leak_subj_201506.txt"
+;                "data/leak/leak_subj_201507.txt"
+;                "data/leak/leak_subj_201508.txt"))
 
-(define tags '("tg_r01g01_data"
-               "tg_r01g02_data"
-               "tg_r01g03_data"
-               "tg_r01g04_data"))
+;(define tags '("tg_r01g01_data"
+;               "tg_r01g02_data"
+;               "tg_r01g03_data"
+;               "tg_r01g04_data"))
+
+(define files '("data/leak/leak_subj_201505.txt"))
+(define tags '("tg_r01g01_data"))
 
 (define sets (map (lambda (x) (reader x)) files))
+
+;(define data- (filter (lambda (x) (member? (car (car x)) excl-words)) (car sets)))
+
 (define sets (map (lambda (x) (counter x)) sets))
-;(define sets (filter (lambda (x) (> (car (cdr x)) 5)) sets))
+
+;(define sets (filter (lambda (x) (> (car (cdr x)) 5)) (car sets)))
+
+;(write sets)
+;(member? "that" excl-words)
+;(car (car sets))
+
+;(define ls (car sets))
+;(write (filter (lambda (x) (not (member? x excl-words))) ls))
+;(car sets)
+
+;(exit)
 
 (define port (open-output-file "report/data/dataTagData.js"))
 
-(define (writer-cloud-wrapper port sets tags)
-        (let loop ((sl sets) (tl tags))            
+(define (writer-cloud-wrapper port reader tags)
+        (let loop ((sl reader) (tl tags))   
             (if (not (null? tl))
              (begin
-              
-              ;(writer-cloud port (car sl) (car tl))
-               (define fs (filter (lambda (x) (> (car (cdr x)) 5)) (car sl)))
-               (writer-cloud port fs (car tl))
 
-               (loop (cdr sl) (cdr tl))
+              ;(writer-cloud port (car sl) (car tl))
+             
+              (let ( (t (car sl)) )                                    
+                   
+                   (let ( (u (car (counter (filter (lambda (x) (not (member? x excl-words))) t))) ) )
+                     
+                     (write u)
+                     ;(write (filter (lambda (x) (> (car (cdr x)) 5) ) (car u)))
+                   
+
+                  )
+                  
+                  ;write (filter (lambda (x) (> (car (cdr x)) 5)) (counter (car sl)) )        
+              )
+              ;(write (car tl))
+
+              ;(writer-cloud port (filter (lambda (x) (> (car (cdr x)) 5)) (car sl)) (car tl))
+              ;(define fs (filter (lambda (x) (> (car (cdr x)) 5)) (car sl)))
+              ;(writer-cloud port fs (car tl))
+
+              (loop (cdr sl) (cdr tl))
              )
             )))
 
+
+
+
+
 (writer-cloud-wrapper port sets tags)
+
 ;(map (lambda (x) (writer-cloud port sets x)) tags)
 
-(close-output-port port)
+;(close-output-port port)
 
 
 
