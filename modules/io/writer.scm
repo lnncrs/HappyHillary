b40aa17a68c8ed041fc01131807f82ac9dd13a7b
lenin cristi <lenincristi@gmail.com>
2016-08-23 16:38:47 -0300

    writer reescrito

--- modules/io/writer.scm
+++ modules/io/writer.scm
@@ -61,3 +61,42 @@
             
             ))
             
+(define writer-cloud
+    (lambda (port values name)
+        ;(let ((port (open-output-file path)))
+            (display (string-append "var " name " = [") port)
+            (newline port)
+            (let f ((ls values))
+                (if (null? (cdr ls))
+                    (if (not (equal? (caar ls) ""))
+                        (begin
+                            (display "{ text: " port)
+                            (write (caar ls) port)
+                            (display ", weight: " port)
+                            (display (+ (cadar ls) 3) port)
+                            (display "}" port)
+                            (newline port)))
+                    (if (equal? (caadr ls) "")
+                        (begin
+                            (display "{ text: " port)
+                            (write (caar ls) port)
+                            (display ", weight: " port)
+                            (display (+ 3 (cadar ls)) port)
+                            (display " }" port)
+                            (newline port)
+                            (f (cdr ls)))
+                        (if (not (or (or (or (or (or (or (equal? (caar ls) "the") (equal? (caar ls) "these")) (equal? (caar ls) "or")) (equal? (caar ls) "and")) (equal? (caar ls) "for")) (equal? (caar ls) "to")) (equal? (caar ls) "---")))
+                            (begin
+                                (display "{ text: " port)
+                                (write (caar ls) port)
+                                (display ", weight: " port)
+                                (display (+ 3 (cadar ls)) port)
+                                (display " }," port)
+                                (newline port)
+                                (f (cdr ls)))))))
+            (display "];" port)
+            (newline port)
+            ;(close-output-port port)
+            ;)
+            ))
+            
--- modules/test-l02.scm
+++ modules/test-l02.scm
@@ -3,11 +3,50 @@
 (require-extension srfi-13)
 (load-relative "lib/utils.scm")
 (load-relative "modules/io/reader.scm")
-(load-relative "modules/io/writer.scm")
+;(load-relative "modules/io/writer.scm")
 (load-relative "modules/workers/counter.scm")
 
 (define file "data/leak/leak_subj_201505.txt")
 (define tag "tg_r01g01_data")
+
+(define writer-cloud
+    (lambda (port values name)
+        ;(let ((port (open-output-file path)))
+            (display (string-append "var " name " = [") port)
+            (newline port)
+            (let f ((ls values))
+                (if (null? (cdr ls))
+                    (if (not (equal? (caar ls) ""))
+                        (begin
+                            (display "{ text: " port)
+                            (write (caar ls) port)
+                            (display ", weight: " port)
+                            (display (+ (cadar ls) 3) port)
+                            (display "}" port)
+                            (newline port)))
+                    (if (equal? (caadr ls) "")
+                        (begin
+                            (display "{ text: " port)
+                            (write (caar ls) port)
+                            (display ", weight: " port)
+                            (display (+ 3 (cadar ls)) port)
+                            (display " }" port)
+                            (newline port)
+                            (f (cdr ls)))
+                        (if (not (or (or (or (or (or (or (equal? (caar ls) "the") (equal? (caar ls) "these")) (equal? (caar ls) "or")) (equal? (caar ls) "and")) (equal? (caar ls) "for")) (equal? (caar ls) "to")) (equal? (caar ls) "---")))
+                            (begin
+                                (display "{ text: " port)
+                                (write (caar ls) port)
+                                (display ", weight: " port)
+                                (display (+ 3 (cadar ls)) port)
+                                (display " }," port)
+                                (newline port)
+                                (f (cdr ls)))))))
+            (display "];" port)
+            (newline port)
+            ;(close-output-port port)
+            ;)
+            ))
 
 ;(define data01 (reader file))
 ;(define data02 (filter (lambda (x) (not (member? x excl-words))) data01))
@@ -25,31 +64,3 @@
 ;(write data04)
 ;(exit)
 
-(define (writer-cloud-wrapper port data tags)
-        (let loop ((sl reader) (tl tags))   
-            (if (not (null? tl))
-             (begin
-
-              ;(writer-cloud port (car sl) (car tl))
-             
-              (let ( (t (car sl)) )                                    
-                   
-                   (let ( (u (car (counter (filter (lambda (x) (not (member? x excl-words))) t))) ) )
-                     
-                     (write u)
-                     ;(write (filter (lambda (x) (> (car (cdr x)) 5) ) (car u)))
-                   
-
-                  )
-                  
-                  ;write (filter (lambda (x) (> (car (cdr x)) 5)) (counter (car sl)) )        
-              )
-              ;(write (car tl))
-
-              ;(writer-cloud port (filter (lambda (x) (> (car (cdr x)) 5)) (car sl)) (car tl))
-              ;(define fs (filter (lambda (x) (> (car (cdr x)) 5)) (car sl)))
-              ;(writer-cloud port fs (car tl))
-
-              (loop (cdr sl) (cdr tl))
-             )
-            )))
