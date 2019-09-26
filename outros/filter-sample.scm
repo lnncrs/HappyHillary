adfcded9a170fae3addd34c524c40389445277d7
lnncrs <lenincristi@gmail.com>
2019-09-25 21:26:38 -0300

    ajuste na documentação

--- outros/filter-sample.scm
+++
@@ -1,3 +0,0 @@
-;(define chars '(#\a #\b #\c #\d #\e #\f))
-;(define excl-chars '(#\b #\c))
-;(display (filter (lambda (x) (not (member? x excl-chars))) chars))
--- outros/report/_original/data/dataTagCall.js
+++
@@ -1,4 +0,0 @@
-$('#tg_r01g01').jQCloud(tg_r01g01_data);
-$('#tg_r01g02').jQCloud(tg_r01g02_data);
-$('#tg_r01g03').jQCloud(tg_r01g03_data);
-$('#tg_r01g04').jQCloud(tg_r01g04_data);
